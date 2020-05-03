bb5064eb53a452d7e39173cd28896d2f2f8a8797
Ubuntu <ubuntu@ip-172-31-35-34.ec2.internal>
2020-05-03 09:44:38 +0000

    committing all files to github

---
+++ js/traversing.js
@@ -0,0 +1,3 @@
+$("button").click(function(){
+		$(this).prev().slideToggle('slow');
+	});
--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-    $("p").click(function(){
+$(document).ready(function() {
+    $("p").click(function() {
         $(this).children("a").css("background-color", "yellow");
+    });
+
+    $("button").click(function() {
+        $(this).prev().slideToggle('slow');
+    });
+
+    $("img").click(function() {
+        $(this).next().children("p").slideDown();
+    });
+
+    $(".card").click(function() {
+        $(this).toggleClass("highlight");
+    });
+
+    $("#select_btn").click(function() {
+        $(".card:not(.highlight)").hide();
+    });
+
+    // Select all cards
+    $("#all_btn").click(function() {
+        $(".card").show();
     });
 });
